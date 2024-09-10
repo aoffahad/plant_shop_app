4c3fc8504dd7fc66eae07296a7880e8987d60f72
aoffahad-admin <omar.faruque@nexdecade.com>
2024-09-10 18:01:15 +0600

    Bug fixed

--- lib/screens/favourite_page.dart
+++ lib/screens/favourite_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:plan_shop_app/screens/category/popular_category.dart';
+import 'package:plan_shop_app/screens/home_page.dart';
 
 import '../helpers/sql_helpers.dart';
+import 'category/popular_category_product_details.dart';
 
 class FavouritePage extends StatefulWidget {
   const FavouritePage({super.key});
@@ -46,24 +49,32 @@ class _FavouritePageState extends State<FavouritePage> {
               itemCount: favouriteItems.length,
               itemBuilder: (context, index) {
                 final item = favouriteItems[index];
-                return ListTile(
-                  // leading: Image.network(item['image']),
-                  leading: Image.asset(item['image']),
-                  title: Text(item['title']),
-                  // subtitle: Text(
-                  //   item['description'],
-                  //   textScaler: TextScaler.linear(1.0),
-                  // ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete, color: Colors.red),
-                    onPressed: () {
-                      setState(() {
-                        _deleteItem(item['image']);
-                        print("Item Deleated");
-                      });
-                    },
+                return GestureDetector(
+                  onTap: () {
+                    // Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //         builder: (context) => PopularCategory()));
+                  },
+                  child: ListTile(
+                    // leading: Image.network(item['image']),
+                    leading: Image.asset(item['image']),
+                    title: Text(item['title']),
+                    // subtitle: Text(
+                    //   item['description'],
+                    //   textScaler: TextScaler.linear(1.0),
+                    // ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      onPressed: () {
+                        setState(() {
+                          _deleteItem(item['image']);
+                          print("Item Deleated");
+                        });
+                      },
+                    ),
+                    subtitle: Text("\$${item['price'].toString()}"),
                   ),
-                  subtitle: Text("\$${item['price'].toString()}"),
                 );
               },
             ),
